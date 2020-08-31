4042161efa3ebc4244ee23b8b4549e6c66c84b0a
Sanjana Chakravarty <sanjana.chakravarty@gmail.com>
2020-08-31 21:18:56 +0530

    Added comments

--- Sanjana/Day1/findDuplicate.py
+++ Sanjana/Day1/findDuplicate.py
@@ -15,3 +15,14 @@ def hareTortoise(arr: [int]) -> int:
 
 arr = [int(x) for x in sys.argv[1:]]
 print("Duplicate: {}".format(hareTortoise(arr)))
+
+'''
+
+The array contains numbers from 1 to n where only one number is present more than once
+
+The duplicate number creates a cycle
+
+Time complexity O(N)
+Space complexity O(1)
+
+'''
