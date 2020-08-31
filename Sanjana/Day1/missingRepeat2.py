9f1f822a5fd812f611639fad50627d843127967c
Sanjana Chakravarty <sanjana.chakravarty@gmail.com>
2020-08-31 21:31:49 +0530

    Added comments

--- Sanjana/Day1/missingRepeat2.py
+++ Sanjana/Day1/missingRepeat2.py
@@ -15,3 +15,30 @@ def repeatMissing(n: int, arr: [int]) -> (int, int):
 n = int(sys.argv[1])
 arr = [int(x) for x in sys.argv[2:]]
 print("The missing number is {} and the duplicate number is {}".format(*repeatMissing(n, arr)))
+
+'''
+
+Array of size n containing numbers from 1 to n. One number is missing and one number is present twice.
+Find missing and duplicate numbers.
+
+S(n) = 1 + 2 + 3 + ... + n = n * (n + 1) // 2
+S2(n) = 1 ** 2 + 2 ** 2 + 3 ** 2 + ... + n ** 2 = n * (n + 1) * (2 * n + 1) // 6
+
+Let s = sum of array elements
+and s2 = sum of sqaured array elements
+
+S(n) - s = missing number - duplicate number
+S2(n) - s2 = (missing number ** 2) - (duplicate number ** 2)
+           = (missing number + duplicate number) * (missing number - duplicate number)
+
+missing number + duplicate number = (S2(n) - s2) // (S(n) - s) = X (let)
+missing number - duplicate number = S(n) - s = Y (let)
+
+Therefore,
+missing number = (X + Y) // 2
+duplicate number = (X - Y) // 2
+
+Time complexity O(N)
+Space complexity O(1)
+
+'''
