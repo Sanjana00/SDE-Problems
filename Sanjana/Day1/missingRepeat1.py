c2831b0a50ab6e3d0f469acfac861740e8128352
Sanjana Chakravarty <53122829+Sanjana00@users.noreply.github.com>
2020-08-31 21:04:09 +0530

    Create missingRepeat1.py

---
+++ Sanjana/Day1/missingRepeat1.py
@@ -0,0 +1,16 @@
+def repeatMissing(n: int, arr: [int]) -> (int, int):
+    freq = {}
+    s = 0
+    S = (n * (n + 1)) // 2
+    for ele in arr:
+        s += ele
+        if ele in freq:
+            dup = ele
+        else:
+            freq[ele] = 1
+    miss = S - s + dup
+    return (miss, dup)
+
+n = int(input())
+arr = [int(x) for x in input().split()]
+print("The missing number is {} and the duplicate number is {}".format(*repeatMissing(n, arr)))
