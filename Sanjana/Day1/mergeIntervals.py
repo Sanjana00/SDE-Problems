5f0d2661ca9fc89d279da5460655e7b243224852
Sanjana Chakravarty <53122829+Sanjana00@users.noreply.github.com>
2020-09-02 21:09:57 +0530

    Create mergeIntervals.py

---
+++ Sanjana/Day1/mergeIntervals.py
@@ -0,0 +1,16 @@
+def is_merge(int1: (int, int), int2: (int, int)) -> bool:
+    return int2[0] <= int1[1]
+
+def merged(int1: (int, int), int2: (int, int)) -> (int, int):
+    return (min(int1[0], int2[0]), max(int1[1], int2[1]))
+
+def mergeIntervals(intervals: [(int, int)]) -> [(int, int)]:
+    intervals.sort()
+    result = [intervals[0]]
+    for interval in intervals:
+        if is_merge(result[-1], interval):
+            result[-1] = merged(result[-1], interval)
+        else:
+            result.append(interval)
+    return result
+    
