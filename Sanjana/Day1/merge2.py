2d39a80120e2b50dd981fc0ae1f87f554f313f67
Sanjana Chakravarty <53122829+Sanjana00@users.noreply.github.com>
2020-09-02 20:01:32 +0530

    Create merge2.py

---
+++ Sanjana/Day1/merge2.py
@@ -0,0 +1,27 @@
+def merge(arr1 : [int], arr2 : [int]) -> [int]:
+    n1, n2 = len(arr1), len(arr2)
+    gap = n1 + n2
+    while gap > 1:
+        gap = (gap + 1) // 2
+        i = 0
+        while i < n1 + n2 - gap:
+            if i < n1:
+                if i + gap < n1:
+                    if arr1[i] > arr1[i + gap]:
+                        arr1[i], arr1[i + gap] = arr1[i + gap], arr1[i]
+                else:
+                    if arr1[i] > arr2[i + gap - n1]:
+                        arr1[i], arr2[i + gap - n1] = arr2[i + gap - n1], arr1[i]
+            else:
+                if arr2[i - n1] > arr2[i + gap - n1]:
+                    arr2[i - n1], arr2[i + gap - n1] = arr2[i + gap - n1], arr2[i - n1]
+            i += 1
+    return arr1 + arr2
+    
+'''
+
+Merging two sorted arrays in sorted order without using extra space
+
+Optimal solution
+
+'''
