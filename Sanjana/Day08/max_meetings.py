963656a24e04d9a129dc4631c60f9cc3d5e6684f
Sanjana Chakravarty <53122829+Sanjana00@users.noreply.github.com>
2020-10-28 21:29:41 +0530

    More pythonic code

--- Sanjana/Day08/max_meetings.py
+++ Sanjana/Day08/max_meetings.py
@@ -1,7 +1,7 @@
 def max_meetings(arr: [(int, int)]) -> int:
     count = 0
     end = 0
-    for finish, start in arr:
+    for start, finish in arr:
         if start >= end:
             count += 1
             end = finish
@@ -9,13 +9,9 @@ def max_meetings(arr: [(int, int)]) -> int:
 
 starts = [int(x) for x in input().split()]
 finishes = [int(x) for x in input().split()]
-size = len(starts)
 
-timestamps = []
-
-for i in range(size):
-    timestamps.append((finishes[i], starts[i]))
+timestamps = list(zip(starts, finishes))
     
-timestamps.sort()
+timestamps.sort(key = lambda x: x[1])
 
 print(max_meetings(timestamps))
