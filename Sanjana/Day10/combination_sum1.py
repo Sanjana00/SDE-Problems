51461d8ae922b5681cca022579bb43041164cb47
Sanjana Chakravarty <53122829+Sanjana00@users.noreply.github.com>
2020-11-27 21:34:07 +0530

    Create combination_sum1.py

---
+++ Sanjana/Day10/combination_sum1.py
@@ -0,0 +1,23 @@
+def combinations(arr, target, collection, result):
+    if target < 0:
+        return
+    if target == 0:
+        result.add(tuple(sorted(collection)))
+        return
+    
+    for element in arr:
+        combinations(arr, target - element, collection + [element], result)
+
+def combinationSum(arr, target):
+    result = set()
+    combinations(arr, target, [], result)
+    
+    return result
+
+arr = [int(x) for x in input().split()]
+target = int(input())
+
+arr.sort()
+
+for combination in combinationSum(arr, target):
+    print(*combination)
